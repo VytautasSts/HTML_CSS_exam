adb4261e9ac6ec31cc52da9fd716d8da6327b99c
VytautasSts <vytas.sts@gmail.com>
2022-11-08 19:20:55 +0200

    Files updated from terminal. No changes made since last upload

--- Task 4/script.js
+++ Task 4/script.js
@@ -7,5 +7,37 @@ nurodomas gamintojas ir jo pagaminti modeliai.
 Pastaba: Sukurta kortelė, kurioje yra informacija apie automobilį (brand), turi 
 būti stilizuota su CSS ir būti responsive;
 -------------------------------------------------------------------------- */
+const ENDPOINT = './cars.json';
 
-const ENDPOINT = 'cars.json';
+async function importData() {
+  const response = await fetch(ENDPOINT);
+  const cars = await response.json();
+  getCars(cars);
+}
+
+function getCars(cars) {
+  console.log(cars)
+  // document.getElementById("loading").style.visibility = "hidden";
+};
+
+importData();
+
+//   function createUserCard (user){  
+
+//     const img = document.createElement('img');
+//     img.src = user.avatar_url;
+//     img.alt = `${user.login} profile picture`;
+
+//     const login = document.createElement('h4');
+//     login.innerText = user.login; 
+
+//     const card = document.createElement('div');
+//     card.append(login, img);
+
+//     document.getElementById("output").append(card);
+//   }
+
+//   function changeView(){
+//     document.getElementById("message").style.visibility ="collapse";
+//     document.getElementById("output").style.textAlign = "center";
+//   };
